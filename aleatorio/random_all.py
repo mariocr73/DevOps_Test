274dc574b183b2a6ff18c67c95e0bef5d839b6bd
NetAcad DEVASC <changeme@example.com>
2021-10-27 13:09:13 +0000

    Ajustes random_all.py ruta de creación de archivos

--- aleatorio/random_all.py
+++ aleatorio/random_all.py
@@ -12,7 +12,8 @@ def rellenar(numero_caracter, nombre_archivo):
   for x in range(number_of_strings):
     K=(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length_of_string)))
   file=open(nombre_archivo,"w")
-  file.write(K)    
+  print("El nombre del archivo creado es: " + nombre_archivo)
+  file.write(K)   
   file.close()
 
 #Funcion para generar el # de caracteres aleatorios  que se insertaran en los archivos
@@ -33,23 +34,14 @@ for i in range(numero_arch):
   if i == numero_arch:
     break
   
-  tf = tempfile.NamedTemporaryFile(mode='w+b', prefix='New', dir='/home/devasc/Documents/prueba-cron/monitor', delete=False)
+  tf = tempfile.NamedTemporaryFile(mode='w+b', prefix='New', dir='/tmp', delete=False)
   l = tf.name
   array_archivos.append(l)
   for j in array_archivos:
-    print(j)
+    #print(j)
     caracter=gen_carac()
     print("El numero de caracteres es: " + str(caracter))
     rellenar(caracter,j)
-    file_size=os.stat('j')            
-    print("Size of file : ", file_size.st_size, "bytes")
-  
-#tempfile — Generate temporary files and directories
+    file_size=os.stat(j)            
+    print("Size of file : ", file_size.st_size, "bytes")  
 
-
-#Mostrar Estadisticas archivos
-#ejemplo_dir = '/home/devasc/Documents/prueba-cron/monitor'
-#directorio = Path(ejemplo_dir)
-#for fichero in directorio.iterdir():
-#    print(fichero.name)
-#    print(os.path.getsize(fichero))
